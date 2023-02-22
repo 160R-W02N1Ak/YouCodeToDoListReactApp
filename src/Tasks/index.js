e2346ac15ac06d3166907e8e36f48a10334b5b55
Igor Woźniak <igor91wozniak@gmail.com>
2023-02-22 16:17:31 +0100

    cleaning

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,7 +3,9 @@ import "./style.css";
 const Tasks = (props) => (
   <ul className="tasksList">
     {props.tasks.map(task => (
-      <li className={`tasks__item${task.done && props.hideDoneTasks ? " tasks__item--hidden" : ""}`}>
+      <li 
+      key={task.id}
+      className={`tasks__item${task.done && props.hideDoneTasks ? " tasks__item--hidden" : ""}`}>
         <button className="tasks__button tasks__button--toggleDone">
           {task.done ? "✓" : ""}
         </button>
