cd31ba6c381771b76e2f13aa18c854a7977ce06e
Igor Woźniak <igor91wozniak@gmail.com>
2023-03-02 19:57:08 +0100

    adding newTask Interaction

--- src/App.js
+++ src/App.js
@@ -39,6 +39,17 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
 
   return (
     <>
@@ -46,7 +57,7 @@ function App() {
         <Header title="Lista zadań" />
         <Section
           title="Dodaj nowe zadanie"
-          body={<Form />}
+          body={<Form addNewTask={addNewTask} />}
         />
         <Section
           title="Lista zadań"
